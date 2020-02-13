13b130de73547f07f4f686979cce685e0f264a36
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-02-13 18:14:11 +0100

    Fix #8302: Move quote tagging to Staging
    Also fixes #8052 and fixes #7521.

--- compiler/src/dotty/tools/dotc/core/StagingContext.scala
+++ compiler/src/dotty/tools/dotc/core/StagingContext.scala
@@ -1,7 +1,12 @@
 package dotty.tools.dotc.core
 
+import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.core.Contexts._
+import dotty.tools.dotc.core.Types._
+import dotty.tools.dotc.core.Symbols._
 import dotty.tools.dotc.core.Contexts._
 import dotty.tools.dotc.util.Property
+import dotty.tools.dotc.transform.PCPCheckAndHeal
 
 import scala.collection.mutable
 
@@ -9,6 +14,8 @@ object StagingContext {
 
   /** A key to be used in a context property that tracks the quoteation level */
   private val QuotationLevel = new Property.Key[Int]
+
+  private val TaggedTypes = new Property.Key[PCPCheckAndHeal.QuoteTypeTags]
 
   /** All enclosing calls that are currently inlined, from innermost to outermost. */
   def level(implicit ctx: Context): Int =
@@ -21,5 +28,12 @@ object StagingContext {
   /** Context with a decremented quotation level. */
   def spliceContext(implicit ctx: Context): Context =
     ctx.fresh.setProperty(QuotationLevel, level - 1)
+
+  def contextWithQuoteTypeTags(taggedTypes: PCPCheckAndHeal.QuoteTypeTags)(implicit ctx: Context) =
+    ctx.fresh.setProperty(TaggedTypes, taggedTypes)
+
+  def getQuoteTypeTags(implicit ctx: Context): PCPCheckAndHeal.QuoteTypeTags =
+    ctx.property(TaggedTypes).get
+
 }
 
