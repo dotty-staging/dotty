dac295301e1fac5df957198801f24944ad97cc85
Nicolas Stucki <nicolas.stucki@gmail.com>
2021-04-23 10:51:44 +0200

    Fix Reflection wildcard abstraction
    Currently, we are missing in the API the Wildcard concept for `case _ =>` patterns.
    These are encoded as term `Ident` with name `_`. This tree can be inconsistently
     matched by `Ident`, `TypeIdent` or `WildcardTypeTree`. There is also no way to create an `Ident(_)`.
    
    The typed expression can contain non-Term trees: Wildcard, Alternatives, Bind and Unapply.
    The solution is to add a TypedTree supertype of Typed that contains a Tree.
    
    Changes
    * `Ident` does not match `Ident(_)`
    * `TypeIdent` does not match `Ident(_)`
    * `WildcardTypeTree` does not match `Ident(_)` if it is a term
    * Add `Wildcard` type that matches a term `Ident(_)`
    * `Typed` only matched if the expr is a `Term`
    * Add `TypedTree`
    
    Fixes #12188

--- project/MiMaFilters.scala
+++ project/MiMaFilters.scala
@@ -5,6 +5,11 @@ import com.typesafe.tools.mima.core.ProblemFilters._
 object MiMaFilters {
   val Library: Seq[ProblemFilter] = Seq(
     // New APIs marked @experimental in 3.0.1
+    exclude[DirectMissingMethodProblem]("scala.quoted.Quotes#reflectModule.TypedTree"),
+    exclude[DirectMissingMethodProblem]("scala.quoted.Quotes#reflectModule.TypedTreeMethods"),
+    exclude[DirectMissingMethodProblem]("scala.quoted.Quotes#reflectModule.TypedTreeTypeTest"),
+    exclude[DirectMissingMethodProblem]("scala.quoted.Quotes#reflectModule.Wildcard"),
+    exclude[DirectMissingMethodProblem]("scala.quoted.Quotes#reflectModule.WildcardTypeTest"),
     exclude[DirectMissingMethodProblem]("scala.quoted.Quotes#reflectModule#SymbolMethods.fieldMember"),
     exclude[DirectMissingMethodProblem]("scala.quoted.Quotes#reflectModule#SymbolMethods.fieldMembers"),
     exclude[DirectMissingMethodProblem]("scala.quoted.Quotes#reflectModule#SymbolMethods.methodMember"),
@@ -14,6 +19,14 @@ object MiMaFilters {
     exclude[DirectMissingMethodProblem]("scala.quoted.Quotes#reflectModule#TermParamClauseMethods.isErased"),
     exclude[MissingClassProblem]("scala.annotation.experimental"),
     exclude[MissingClassProblem]("scala.annotation.internal.ErasedParam"),
+    exclude[MissingClassProblem]("scala.quoted.Quotes$reflectModule$TypedTreeMethods"),
+    exclude[MissingClassProblem]("scala.quoted.Quotes$reflectModule$TypedTreeModule"),
+    exclude[MissingClassProblem]("scala.quoted.Quotes$reflectModule$WildcardModule"),
+    exclude[ReversedMissingMethodProblem]("scala.quoted.Quotes#reflectModule.TypedTree"),
+    exclude[ReversedMissingMethodProblem]("scala.quoted.Quotes#reflectModule.TypedTreeMethods"),
+    exclude[ReversedMissingMethodProblem]("scala.quoted.Quotes#reflectModule.TypedTreeTypeTest"),
+    exclude[ReversedMissingMethodProblem]("scala.quoted.Quotes#reflectModule.Wildcard"),
+    exclude[ReversedMissingMethodProblem]("scala.quoted.Quotes#reflectModule.WildcardTypeTest"),
     exclude[ReversedMissingMethodProblem]("scala.quoted.Quotes#reflectModule#SymbolMethods.fieldMember"),
     exclude[ReversedMissingMethodProblem]("scala.quoted.Quotes#reflectModule#SymbolMethods.fieldMembers"),
     exclude[ReversedMissingMethodProblem]("scala.quoted.Quotes#reflectModule#SymbolMethods.methodMember"),
