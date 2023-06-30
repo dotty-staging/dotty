d75d66c430c0c6da016fe0fe628c672c3784501c
Ondrej Lhotak <olhotak@uwaterloo.ca>
2023-06-30 15:12:38 -0400

    make derivedFlexibleType reuse this when unchanged

--- compiler/src/dotty/tools/dotc/core/Types.scala
+++ compiler/src/dotty/tools/dotc/core/Types.scala
@@ -3422,7 +3422,7 @@ object Types extends TypeUtils {
     override def show(using Context) = "FlexibleType("+tp.show+")"
     def underlying(using Context) : Type = this.tp
     def derivedFlexibleType(under: Type)(using Context): Type =
-      FlexibleType(under)
+      if this.tp eq under then this else FlexibleType(under)
     override def computeHash(bs: Binders): Int = doHash(bs, tp)
     override def toString = "FlexibleType(%s)".format(tp)
     //override def hash = NotCached
