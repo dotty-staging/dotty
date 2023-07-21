79910d5ed90ac984b4d9f4d069560f0f132623bd
Ondrej Lhotak <olhotak@uwaterloo.ca>
2023-07-21 13:36:40 -0400

    undo spurious edit

--- compiler/src/dotty/tools/dotc/core/Types.scala
+++ compiler/src/dotty/tools/dotc/core/Types.scala
@@ -1672,10 +1672,6 @@ object Types extends TypeUtils {
 
     /** The type <this . name> with given denotation, reduced if possible. */
     def select(name: Name, denot: Denotation)(using Context): Type =
-      /*println("select:")
-      println(name)
-      println(denot)
-      println(NamedType(this, name, denot).reduceProjection) // "method get" for level12.get(0)*/
       NamedType(this, name, denot).reduceProjection
 
     /** The type <this . sym>, reduced if possible */
