ff6a923ac478cee1b0269b398d620aca13f6ded9
Yichen Xu <yichen.x@outlook.com>
2023-09-09 00:57:43 +0200

    Fix test: sep-basics.scala

--- tests/neg-custom-args/captures/sep-basics.scala
+++ tests/neg-custom-args/captures/sep-basics.scala
@@ -3,18 +3,18 @@ import language.experimental.captureChecking
 trait Ref:
   def use(): Unit
 
-// def f(x: Ref^, sep{x} y: Ref^): Unit = ()
-// def g(x: Ref^, sep{x} y: Ref^, sep{x, y} z: Ref^): Unit = ()
-// def h(x: Ref^, sep{} y: Ref^{x}): Unit = ()
-// def foo(x: Ref^, sep y: Ref^): Unit = ()
+def f(x: Ref^, sep{x} y: Ref^): Unit = ()
+def g(x: Ref^, sep{x} y: Ref^, sep{x, y} z: Ref^): Unit = ()
+def h(x: Ref^, sep{} y: Ref^{x}): Unit = ()
+def foo(x: Ref^, sep y: Ref^): Unit = ()
 
 def check[T, U](x: () => T)(sep{x} y: () => U): Unit = ()
 
-// def test1(a: Ref^, sep{a} b: Ref^, sep{a, b} c: Ref^): Unit =
-//   check(() => a)(() => b)
-//   check(() => a)(() => a) // error
-//   check(() => { a.use(); b.use() })(() => c)
-//   check(() => { a.use(); b.use() })(() =>{ b.use(); c.use()  }) // error
+def test1(a: Ref^, sep{a} b: Ref^, sep{a, b} c: Ref^): Unit =
+  check(() => a)(() => b)
+  check(() => a)(() => a) // error
+  check(() => { a.use(); b.use() })(() => c)
+  check(() => { a.use(); b.use() })(() =>{ b.use(); c.use()  }) // error
 
 def test2(a: Ref^, sep{a} b: Ref^, sep{a, b} c: Ref^): Unit =
   val f1 = () => a.use()
