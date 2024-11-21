48b594c766b5203b1e681785dc1ff67ec42e4674
Kacper Korban <kacper.f.korban@gmail.com>
2024-11-21 11:55:55 +0100

    Add a tracked lint test with overriding explicit tracked

--- tests/warn/lint-tracked.scala
+++ tests/warn/lint-tracked.scala
@@ -7,7 +7,16 @@ abstract class C:
   type T
   def foo: T
 
-class F(val x: C): // x is infered to be tracked
+class F0(val x: C): // x is infered to be tracked
   val result: x.T = x.foo
 
-class G(override val x: C) extends F(x) // warn
+class F1(tracked val x: C):
+  val result: x.T = x.foo
+
+class G0(val y: C) extends F0(y) // warn
+
+class G00(override val x: C) extends F0(x) // warn
+
+class G1(override val x: C) extends F1(x) // warn
+
+class G11(val y: C) extends F1(y)
