3a89fc5ab61958b942d1e43da2d5a4c4f524bfee
odersky <odersky@gmail.com>
2025-03-11 17:54:32 +0100

    Add `mut` modifier to Parser doc comment

--- compiler/src/dotty/tools/dotc/parsing/Parsers.scala
+++ compiler/src/dotty/tools/dotc/parsing/Parsers.scala
@@ -3386,7 +3386,8 @@ object Parsers {
      *                  |  override
      *                  |  opaque
      *  LocalModifier  ::= abstract | final | sealed | open | implicit | lazy | erased |
-     *                     inline | transparent | infix
+     *                     inline | transparent | infix |
+     *                     mut                              -- under cc
      */
     def modifiers(allowed: BitSet = modifierTokens, start: Modifiers = Modifiers()): Modifiers = {
       @tailrec
