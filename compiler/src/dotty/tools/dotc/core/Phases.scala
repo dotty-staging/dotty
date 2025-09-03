b3c263e88779bd86eb72237190e8a359049730b9
Jan Chyb <jan.chyb.99@gmail.com>
2025-09-03 16:18:27 +0200

    Bring back previous typerState after suspending a compilation unit
    This is to avaoid unfulfilled constraints when
    going through -Ycheck later.
    
    [Cherry-picked 6b72d4cc4d3c6a42b0915d627e2a65be02600965]

--- compiler/src/dotty/tools/dotc/core/Phases.scala
+++ compiler/src/dotty/tools/dotc/core/Phases.scala
@@ -336,12 +336,14 @@ object Phases {
 
       for unit <- units do ctx.profiler.onUnit(this, unit):
         given unitCtx: Context = runCtx.fresh.setPhase(this.start).setCompilationUnit(unit).withRootImports
+        val previousTyperState = unitCtx.typerState.snapshot()
         if ctx.run.enterUnit(unit) then
           try
             run
             buf += unitCtx.compilationUnit
           catch
             case _: CompilationUnit.SuspendException => // this unit will be run again in `Run#compileSuspendedUnits`
+              unitCtx.typerState.resetTo(previousTyperState)
             case ex: Throwable if !ctx.run.enrichedErrorMessage =>
               println(ctx.run.enrichErrorMessage(s"unhandled exception while running $phaseName on $unit"))
               throw ex
