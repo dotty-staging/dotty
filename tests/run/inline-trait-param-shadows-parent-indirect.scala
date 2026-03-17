3c3b63daa91154c536abbf369cbf0bdc9840f0ad
Hamish <hamishstarling@hotmail.co.uk>
2026-03-17 12:38:55 +0100

    Clean up test

--- tests/run/inline-trait-param-shadows-parent-indirect.scala
+++ tests/run/inline-trait-param-shadows-parent-indirect.scala
@@ -1,12 +1,14 @@
 inline trait A[T](x: T):
     def y = x
+
 trait B extends A[Int]
 trait D extends A[Int]
+
+// These two are not really necessary for the case but add to the "indirectness"
 trait E extends B
 trait F extends D
-class C extends E, F
-// A[Int](4)
 
+class C extends E, F
 
 object Test:
     def main(args: Array[String]): Unit = 
