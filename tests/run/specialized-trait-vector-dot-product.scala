ae18157341cad9a96e4418a83ab00fe6039d0845
Hamish <hamishstarling@hotmail.co.uk>
2026-03-22 13:48:05 +0100

    Add dot product run test

---
+++ tests/run/specialized-trait-vector-dot-product.scala
@@ -0,0 +1,22 @@
+inline trait Vec[T: {Specialized, Numeric}](elems: Array[T]):
+  private val num = summon[Numeric[T]]
+  private val x = 1
+
+  def length = elems.length
+
+  def apply(i: Int): T = elems(i)
+
+  def scalarProduct(other: Vec[T]): T =
+    require(this.length == other.length)
+    var result = num.fromInt(0)
+    for i <- 0 until length do
+      result = num.plus(result, num.times(this(i), other(i)))
+    result
+
+object Test:
+  def main(args: Array[String]) = 
+    val x = new Vec[Int](Array(1, 2, 3, 4, 5)) {}
+    val y = new Vec[Int](Array(3, 4, 5, 6, 7)) {}
+    val z = x.scalarProduct(y)
+    assert(z == 85)
+  
