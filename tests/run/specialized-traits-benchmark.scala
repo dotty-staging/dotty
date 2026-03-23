c857661f1d087679c8f7614cd82aa42c20e0a9e3
Hamish <hamishstarling@hotmail.co.uk>
2026-03-23 17:24:48 +0100

    Replace manual benchmark with scala-cli --jmh compatible benchmark

---
+++ tests/run/specialized-traits-benchmark.scala
@@ -0,0 +1,84 @@
+// Run with: scala-cli --power --jmh dotty/tests/run/specialized-traits-benchmark.scala
+// May have to run it again if you get a class not found error from scala-cli first time.
+//> using scala 3.8.3-RC1-bin-SNAPSHOT-nonbootstrapped
+
+package bench
+
+import org.openjdk.jmh.annotations.*
+import java.util.concurrent.TimeUnit
+
+class VecManual(elems: Array[Int]):
+  private val num = summon[Numeric[Int]]
+  private val x = 1
+
+  def length = elems.length
+
+  def apply(i: Int): Int = elems(i)
+
+  def scalarProduct(other: VecManual): Int =
+    require(this.length == other.length)
+    var result = num.fromInt(0)
+    for i <- 0 until length do
+      result = num.plus(result, num.times(this(i), other(i)))
+    result
+
+class VecGeneric[T: Numeric](elems: Array[T]):
+  private val num = summon[Numeric[T]]
+  private val x = 1
+
+  def length = elems.length
+
+  def apply(i: Int): T = elems(i)
+
+  def scalarProduct(other: VecGeneric[T]): T =
+    require(this.length == other.length)
+    var result = num.fromInt(0)
+    for i <- 0 until length do
+      result = num.plus(result, num.times(this(i), other(i)))
+    result
+
+inline trait VecSpecialized[T: {Specialized, Numeric}](elems: Array[T]):
+  private val num = summon[Numeric[T]]
+  private val x = 1
+
+  def length = elems.length
+
+  def apply(i: Int): T = elems(i)
+
+  def scalarProduct(other: VecSpecialized[T]): T =
+    require(this.length == other.length)
+    var result = num.fromInt(0)
+    for i <- 0 until length do
+      result = num.plus(result, num.times(this(i), other(i)))
+    result
+
+@State(Scope.Benchmark)
+class Arrays:
+  var arr1 = Array.fill(100_000_000) {math.round(math.random().floatValue * 4)}
+  var arr2 = Array.fill(100_000_000) {math.round(math.random().floatValue * 4)}
+  val target = arr1.zip(arr2).map((x, y) => x * y).fold(0)(_ + _)
+  
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 15, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(1)
+class VecBench:
+  @Benchmark
+  def manual(arr: Arrays) =
+    val x = VecManual(arr.arr1)
+    val y = VecManual(arr.arr2)
+    assert(x.scalarProduct(y) == arr.target)
+
+  @Benchmark
+  def generic(arr: Arrays) =
+    val x = VecGeneric[Int](arr.arr1)
+    val y = VecGeneric[Int](arr.arr2)
+    assert(x.scalarProduct(y) == arr.target)
+
+  @Benchmark
+  def specialized(arr: Arrays) =
+    val x = new VecSpecialized[Int](arr.arr1) {}
+    val y = new VecSpecialized[Int](arr.arr2) {}
+    assert(x.scalarProduct(y) == arr.target)
